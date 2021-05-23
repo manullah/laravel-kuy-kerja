839916ae1f232a2506f93f6147153967c6f23e73
Muhammad Imanullah <pengguna21@gmail.com>
2021-05-23 07:58:38 +0700

    Fix | component pagination-links

--- app/View/Components/PaginationLinks.php
+++ app/View/Components/PaginationLinks.php
@@ -4,7 +4,7 @@ namespace App\View\Components;
 
 use Illuminate\View\Component;
 
-class pagination-links extends Component
+class PaginationLinks extends Component
 {
     /**
      * Create a new component instance.
